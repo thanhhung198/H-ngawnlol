f144add7d9229734158a77d5b288e05e6f33aa4e
hung nguyen <thanhhung98hn@gmail.com>
2020-04-01 17:38:36 +0700

    aa

---
+++ src/main/java/com/caigicungco/Main.java
@@ -0,0 +1,24 @@
+package com.caigicungco;
+
+public class Main {
+    static class A{
+        int a;
+        int b;
+
+
+        @Override
+        public String toString() {
+            return a + " Hung Dz";
+        }
+
+        public String toNull(){
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+       int a = 01200;
+        System.out.println(a);
+
+    }
+}
